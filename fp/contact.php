cb825021b0df5a678c94d6c0638dabd11ede4399
Zhongyuan Huang <zhongyuan.huang1996@gmail.com>
2022-06-17 00:12:10 -0700

    Final

--- fp/contact.php
+++ fp/contact.php
@@ -11,24 +11,20 @@
 
  </head>
  <body>
-     <header>
-     <h1>Zhongyuan: Contact Form</h1>
-     <nav class="topnav" id="myTopnav">
-       <a href="index.html">Welcome</a>
-       <a href="elements.html">HTML Elements</a>
-       <a href="aia.html">AIA</a>
-       <a href="big/index.html">BIG</a>
-       <a href="http://w3schools.com" target="_blank">W3 Schools</a>
-       <a href="https://www.linkedin.com/learning/" target="_blank">LinkedIn Learning</a>
-       <a href="flowchart.html">Flowchart</a>
-       <a href="fp/template.html">Final Project</a>
-       <a href="contact.php" class="active">Contact Zhongyuan</a>
-       <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
-     </nav>
-     </header>
+    <header>
+        <h1> &nbsp; </h1>
+        <nav class="topnav" id="myTopnav">
+                <a href="template.html">Home</a>
+                <a href="gallery.html" >Gallery</a>
+                <div class="logo"></div>
+                <a href="menu.html">Menu</a>
+                <a href="contact.php" class="active">Contact</a>  
+
+        </nav>
+    </header>
      
    <div class="wrapper">
-        <h2 class="subheader">Contact Zhongyuan</h2>
+        <h2 class="subheader" >Contact us!</h2>
         	<!-- START HTML FORM -->
 	<form action="contact.php" method="post">
 	<div>
@@ -83,12 +79,37 @@
         }); 
     </script>     
     
-     <footer>
-      <p><small>&copy; 2022 by 
-          <a href="contact.php">Zhongyuan</a>, All Rights Reserved ~ 
-          <a id="html-checker" href="#">Check HTML</a> ~ 
-          <a id="css-checker" href="#">Check CSS</a></small>
-     </p>
+    <footer>
+        <div class="contact-info">
+            <div>
+            <h2>Contact imformation</h2>
+            <ul style="list-style-type:none;">
+                <li>81 Chimacum Rd, Port Hadlock-Irondale, WA 98339</li>
+                <li>(360) 379-1009</li>
+                <li> Monday, Wednesday - Friday: 11AM-9PM <br> Saturday &amp; Sunday: 12-9PM  </li>
+            </ul>
+            </div>
+
+
+            <div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2668.1100823919774!2d-122.76113468435474!3d48.030900579215086!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x548fe8ca3f0e9065%3A0x83537f822c8c811a!2sJoy%20Luck%20Restaurant!5e0!3m2!1sen!2sus!4v1655270869605!5m2!1sen!2sus" 
+                style="border: 0;;"
+                allowfullscreen="" 
+                loading="lazy" 
+                referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
+            </div>
+
+
+
+        </div>
+
+        <p><small>&copy; 2022 by 
+            Zhongyuan, All Rights Reserved ~ 
+            <a id="html-checker" href="#" target="_blank">Check HTML</a> ~ 
+            <a id="css-checker" href="#" target="_blank">Check CSS</a>~
+            <a link href="disclaimer.html">Disclaimer</a></small>
+        </p>
     </footer>
   </div>
      
