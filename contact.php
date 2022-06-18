0e10d92b255d83957882883bba27c765adf1a455
Zhongyuan Huang <zhongyuan.huang1996@gmail.com>
2022-06-17 17:27:54 -0700

    final

--- contact.php
+++ contact.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
  <head>
@@ -29,59 +30,25 @@
      
    <div class="wrapper">
         <h2 class="subheader">Contact Zhongyuan</h2>
-        	<!-- START HTML FORM -->
-	<form action="contact.php" method="post">
-	<div>
-		<label>
-			Name:<br /><input type="text" name="Name" required="required" placeholder="Full Name (required)" title="Name is required" tabindex="10" size="44" autofocus />
-		</label>
-	</div>
-	<div>	
-		<label>
-			Email:<br /><input type="email" name="Email" required="required" placeholder="Email (required)" title="A valid email is required" tabindex="20" size="44" />
-		</label>
-	</div>
-	<!-- below change the HTML to your form elements - only 'Name' & 'Email' (above) are significant -->
-	<div>	
-		<label>
-			Comments:<br /><textarea name="Comments" cols="36" rows="4" placeholder="Your comments are important to us!" tabindex="30"></textarea>
-		</label>
-	</div>	
-    <div class="g-recaptcha" data-sitekey="6LeDaSoUAAAAACnEiqA3QAkiRU-Q_wtk0vuBa_OX"></div> 
-	<div>
-		<input type="submit" value="submit" />
-	</div>
-    </form>
-	<!-- END HTML FORM -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script type="text/javascript" src="https://www.google.com/recaptcha/api.js?hl=en"></script>
-    <script>
-        /**
-        * requires reCAPTCHA so user doesn't lose form data 
-        *
-        * https://stackoverflow.com/questions/27706594/how-can-i-make-recaptcha-a-required-field
-        * 
-        * In this version, jQuery is required and code will find first form on page and require 
-        * the reCAPTCHA for that form using document.forms[0]
-        */
+        <?php
+        /*
+         * Below are 2 different forms to be re-used       
+         * 
+         * Only use one at a time, comment out the other!       
+         *
+         */
+
+        include 'includes/contact_include.php'; #site keys & code here
     
-        function checkRecaptcha() {
-            res = $('#g-recaptcha-response').val();
-            if (res == "" || res == undefined || res.length == 0)
-                return false;
-            else
-                return true;
-        }
-        $(function() {
-            let thisForm = document.forms[0];
-            $(thisForm).submit(function(e) {            
-                if(!checkRecaptcha()) {
-                    alert("Please confirm you are not a robot.");
-                    return false;
-                }
-            });
-        }); 
-    </script>     
+        $toAddress = "zhongyuan.huang1996@gmail.com";  //place your/your client's email address here
+        $toName = "zhongyuan"; //place your client's name here
+        $website = "testingpage";  //place NAME of your client's website
+
+        echo loadContact('simple.php');#demonstrates a simple contact form
+        //echo loadContact('multiple.php');#demonstrates multiple form elements
+
+	?>
+     
     
      <footer>
       <p><small>&copy; 2022 by 
--- fp/contact.php
+++ fp/contact.php
@@ -12,7 +12,7 @@
  </head>
  <body>
     <header>
-        <h1> &nbsp; </h1>
+        <h1 class="toppic"> &nbsp; </h1>
         <nav class="topnav" id="myTopnav">
                 <a href="template.html">Home</a>
                 <a href="gallery.html" >Gallery</a>
@@ -24,60 +24,25 @@
     </header>
      
    <div class="wrapper">
-        <h2 class="subheader" >Contact us!</h2>
-        	<!-- START HTML FORM -->
-	<form action="contact.php" method="post">
-	<div>
-		<label>
-			Name:<br /><input type="text" name="Name" required="required" placeholder="Full Name (required)" title="Name is required" tabindex="10" size="44" autofocus />
-		</label>
-	</div>
-	<div>	
-		<label>
-			Email:<br /><input type="email" name="Email" required="required" placeholder="Email (required)" title="A valid email is required" tabindex="20" size="44" />
-		</label>
-	</div>
-	<!-- below change the HTML to your form elements - only 'Name' & 'Email' (above) are significant -->
-	<div>	
-		<label>
-			Comments:<br /><textarea name="Comments" cols="36" rows="4" placeholder="Your comments are important to us!" tabindex="30"></textarea>
-		</label>
-	</div>	
-    <div class="g-recaptcha" data-sitekey="6LeDaSoUAAAAACnEiqA3QAkiRU-Q_wtk0vuBa_OX"></div> 
-	<div>
-		<input type="submit" value="submit" />
-	</div>
-    </form>
-	<!-- END HTML FORM -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script type="text/javascript" src="https://www.google.com/recaptcha/api.js?hl=en"></script>
-    <script>
-        /**
-        * requires reCAPTCHA so user doesn't lose form data 
-        *
-        * https://stackoverflow.com/questions/27706594/how-can-i-make-recaptcha-a-required-field
-        * 
-        * In this version, jQuery is required and code will find first form on page and require 
-        * the reCAPTCHA for that form using document.forms[0]
-        */
+        <h1 class="subheader" >Contact us!</h1>
+        <?php
+        /*
+         * Below are 2 different forms to be re-used       
+         * 
+         * Only use one at a time, comment out the other!       
+         *
+         */
+
+        include 'includes/contact_include.php'; #site keys & code here
     
-        function checkRecaptcha() {
-            res = $('#g-recaptcha-response').val();
-            if (res == "" || res == undefined || res.length == 0)
-                return false;
-            else
-                return true;
-        }
-        $(function() {
-            let thisForm = document.forms[0];
-            $(thisForm).submit(function(e) {            
-                if(!checkRecaptcha()) {
-                    alert("Please confirm you are not a robot.");
-                    return false;
-                }
-            });
-        }); 
-    </script>     
+        $toAddress = "joyluck50@yahoo.com";  //place your/your client's email address here
+        $toName = "Joy Luck"; //place your client's name here
+        $website = "Joy Luck Website";  //place NAME of your client's website
+
+        echo loadContact('simple.php');#demonstrates a simple contact form
+        //echo loadContact('multiple.php');#demonstrates multiple form elements
+
+	?>   
     
     <footer>
         <div class="contact-info">
